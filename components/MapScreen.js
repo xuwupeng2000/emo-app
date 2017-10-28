520c177e57c4b564b1629075fbb927ad468da8cd
Jack Wu( [♠]]] ) <xuwupeng2000@gmail.com>
2017-10-28 21:22:53 +1300

    Get current location

--- components/MapScreen.js
+++ components/MapScreen.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react'
+import { Dimensions } from 'react-native'
 import { MapView } from 'expo';
+
+let { width, height } = Dimensions.get('window');
+const ASPECT_RATIO = width / height;
+const LATITUDE = 0;
+const LONGITUDE = 0;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 export default class MapScreen extends Component {
 
+  constructor(){
+    super();
+
+    this.state = { 
+      region: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      }
+    };
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          region: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          }
+        });
+      }, (err) => {
+
+      }
+    );
+  }
+
   render() {
     return (
-      <MapView
-        style={{ flex: 1 }}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
+      <MapView style={{flex: 1}}
+        region={ this.state.region }
       />
     )
   }
